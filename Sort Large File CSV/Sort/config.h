4cbe6f224e501423136a231adaac29388ffd8a7d
Duc Hoai Nguyen <nguyenduchoai25032003@gmail.com>
2022-12-06 16:48:15 +0700

    completed

--- Sort Large File CSV/Sort/config.h
+++ Sort Large File CSV/Sort/config.h
@@ -7,6 +7,7 @@
 #include <fstream>
 #include <sstream>
 #include <stack>
+#include <ctime>
 
 using namespace std;
 
--- Sort Large File CSV/Sort/main.cpp
+++ Sort Large File CSV/Sort/main.cpp
@@ -2,15 +2,22 @@
 
 string INPUT_FILE = "Books_rating.csv";
 string OUTPUT_FILE = "sorted_books_rating.csv";
-long long INTERNAL_MEMORY_LIMIT = 204800000;
+long long INTERNAL_MEMORY_LIMIT = 204800000; // 200MB
+
+const clock_t timeBegin = clock();
+
+// run program on mode "Release" in Visual Studio (version current: 2022)
 
 int main()
 {
     int fileCount = preProcessing(INPUT_FILE, INTERNAL_MEMORY_LIMIT);
 
+    cout << "Time for preprocessing: " << double(clock() - timeBegin) / CLOCKS_PER_SEC * 1000 << "miliseconds\n\n";
+
     cout << "Merging " << fileCount << " files into file " << OUTPUT_FILE << "\n\n";
 
     mergeTotalFile(fileCount, OUTPUT_FILE, INTERNAL_MEMORY_LIMIT);
 
+    cout << "Time total of propram: " << double(clock() - timeBegin) / CLOCKS_PER_SEC * 1000 << "miliseconds\n\n";
     return 0;
 }
